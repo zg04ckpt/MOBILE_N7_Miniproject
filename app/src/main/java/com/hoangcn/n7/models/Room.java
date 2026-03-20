7e67c48d1dc610eaf00445f068ac75ec554a0b28
buicongbac19 <buicongbac192004@gmail.com>
2026-03-20 14:43:47 +0700

    Done UI for list room

--- app/src/main/java/com/hoangcn/n7/models/Room.java
+++ app/src/main/java/com/hoangcn/n7/models/Room.java
@@ -1,11 +1,72 @@
 package com.hoangcn.n7.models;
 
-public class Room {
-    private int id;
+import java.io.Serializable;
+
+public class Room implements Serializable {
+    private String id;
     private String name;
-    private float price;
-    private String status;
-    private String tenant;
-    private String phoneNumber;
-    private int previewImage;
+    private double price;
+    private boolean isRented;
+    private String tenantName;
+    private String tenantPhone;
+
+    public Room() {
+    }
+
+    public Room(String id, String name, double price, boolean isRented, String tenantName, String tenantPhone) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.isRented = isRented;
+        this.tenantName = tenantName;
+        this.tenantPhone = tenantPhone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+    public String getTenantName() {
+        return tenantName;
+    }
+
+    public void setTenantName(String tenantName) {
+        this.tenantName = tenantName;
+    }
+
+    public String getTenantPhone() {
+        return tenantPhone;
+    }
+
+    public void setTenantPhone(String tenantPhone) {
+        this.tenantPhone = tenantPhone;
+    }
 }
