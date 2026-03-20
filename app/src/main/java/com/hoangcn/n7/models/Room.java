12c94122a44e242147331659bbf7bcd132b61ec6
zg04ckpt <nguyenhc424@gmail.com>
2026-03-20 14:21:39 +0700

    init base project

---
+++ app/src/main/java/com/hoangcn/n7/models/Room.java
@@ -0,0 +1,11 @@
+package com.hoangcn.n7.models;
+
+public class Room {
+    private int id;
+    private String name;
+    private float price;
+    private String status;
+    private String tenant;
+    private String phoneNumber;
+    private int previewImage;
+}
